bc87f0aad8f376654b5da88f6b517c78753e6857
Riead <mdriead.bd@gmail.com>
2024-02-23 11:16:02 +0600

    task 1 done

--- module-30/task1.js
+++ module-30/task1.js
@@ -10,4 +10,4 @@ I love to eat briyani.`;
 
 // 3. Write an arrow function that will take 2 parameters. One parameter will come from you and the other parameter will be a default parameter. Add these two parameters and returns the result.
 const sum = (a, b=5) => a + b;
-console.log(sum(15));
+// console.log(sum(15));
